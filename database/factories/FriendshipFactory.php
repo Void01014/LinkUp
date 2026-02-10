376379cd55e9eea022c2b8f2ea9e7edb06935532
ouichouani <abdelhakim@gmail.com>
2026-02-11 00:03:05 +0100

    SSS:44 #Done

--- database/factories/FriendshipFactory.php
+++ database/factories/FriendshipFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Friendship;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User ;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Friendship>
@@ -14,10 +16,26 @@ class FriendshipFactory extends Factory
      *
      * @return array<string, mixed>
      */
+
+    protected $model = Friendship::class ;
+
     public function definition(): array
     {
+
+
+        $reseaver = User::inRandomOrder()->first()->id ;
+        $sender = User::inRandomOrder()->first()->id ;
+
+        while($reseaver === $sender){
+            $reseaver = User::inRandomOrder()->first()->id ;
+        }
+
+
         return [
-            //
+            'user_id' => $sender ,
+            'friend_id' => $reseaver ,
+            'status' => fake()->randomElement(['pending', 'accepted', 'rejected'])
         ];
     }
+
 }
