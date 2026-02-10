b36896011c8168727248574c84916f307cae0e73
ouichouani <abdelhakim@gmail.com>
2026-02-11 00:03:05 +0100

    SSS-44 #Done create seeders and factory

--- database/factories/LikeFactory.php
+++ database/factories/LikeFactory.php
@@ -3,6 +3,9 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Like;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Like>
@@ -14,10 +17,25 @@ class LikeFactory extends Factory
      *
      * @return array<string, mixed>
      */
+
+    static $used = [] ;
+
+
     public function definition(): array
     {
+        do {
+
+            $user_id = User::inRandomOrder()->first()->id ;
+            $post_id = Post::inRandomOrder()->first()->id ;
+            
+        } while (isset(self::$used[$user_id. '_' . $post_id]));
+        
+        self::$used[] = $user_id. '_' . $post_id ;
+
         return [
-            //
+            'user_id' => $user_id,
+            'post_id' => $post_id,
         ];
+
     }
 }
