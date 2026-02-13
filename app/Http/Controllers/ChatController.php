c331d33e3b78e0f4316329aed7c4e731f0de20c2
Omar EL FILALI <omarelfiie2007@gmail.com>
2026-02-13 12:52:51 +0100

    I changed the whole 'Chat' system, Now I'll move to the testing phase

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,31 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Conversation;
 use App\Models\Message;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ChatController extends Controller
 {
     public function inbox($id = null)
     {
-        $conversations = User::where('status');
-        $activeConversation = User::find($id) ?? [];
-        $messages = Message::where(function ($query) use ($id) {
-            $query->where('sender_id', auth()->id())
-                ->where('receiver_id', $id);
-        })->orWhere(function ($query) use ($id) {
-            $query->where('sender_id', $id)
-                ->where('receiver_id', auth()->id());
-        })
-            ->orderBy('created_at', 'asc')
-            ->get();
+        $conversations = User::all();
+        $activeConversation = Conversation::getConversation(Auth::id(), $id);
 
         return view('chat.inbox', [
             'conversations' => $conversations,
             'activeConversation' => $activeConversation,
-            'messages' => $messages,
+            'messages' => $activeConversation->messages,
             'user_id' => auth()->user()->id,
+            'user_one' => $activeConversation->user_one,
+            'user_two' => $activeConversation->user_two,
         ]);
     }
 }
--- routes/channels.php
+++ routes/channels.php
@@ -7,5 +7,5 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
 });
 
 Broadcast::channel('chat.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    return true;
 });
